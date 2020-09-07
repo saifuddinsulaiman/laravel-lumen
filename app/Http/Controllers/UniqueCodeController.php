0494f809efe77b8795f37005f5a5cd2309c1f841
saifuddin <msaifuddinsulaiman@gmail.com>
2020-09-08 03:10:05 +0800

    upload project

---
+++ app/Http/Controllers/UniqueCodeController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\ModelUniqueCode;
+use Illuminate\Http\Request;
+use DB;
+use Carbon\Carbon;
+
+class UniqueCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = ModelUniqueCode::all();
+        return response($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $uniquenumber = json_decode($request->input('uniquenumber'));
+
+        foreach($uniquenumber as $uniquenumber)
+        {   
+            $uniquenumber_records[] = [
+                    'uniquenumber' => $uniquenumber 
+                ];
+        }
+
+        
+
+        ModelUniqueCode::insert($uniquenumber_records);
+
+
+        return response($uniquenumber );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UniqueCode  $uniqueCode
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = ModelUniqueCode::where('id',$id)->get();
+        return response ($data);
+    }
+
+}
---
+++ app/ModelUniqueCode.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelUniqueCode extends Model
+{
+   protected $table = 'mynumber';
+}
